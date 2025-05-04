83d2944b7af69c575d4a6ec3a729d6a82cd86db2
fgaschick <felix.gaschick@gmail.com>
2025-05-04 17:01:45 +0600

    Update CFED_AI_Tool_App_FIXED.py

--- CFED_AI_Tool_App_FIXED.py
+++ CFED_AI_Tool_App_FIXED.py
@@ -78,7 +78,6 @@ st.markdown("""
     <div class='header-bar'>
     <img src='https://raw.githubusercontent.com/fgaschick/cfed-ai-tool/main/Chemonics_RGB_Horizontal_BLUE-WHITE.png' alt='Chemonics Logo'/>
 </div>
-</div>
 <div style='height: 100px;'></div>
     <div class='custom-footer'>
         © 2025 Chemonics International Inc. | Contact: Climate Finance Team
